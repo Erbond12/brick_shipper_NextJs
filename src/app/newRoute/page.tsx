8db31b15f5c3e80055a85221e131d08d8e6d7d54
Erik MbP <alerik8@yahoo.de>
2024-02-20 22:39:17 +0100

    add: basic pages for settings, homepage and new route (last is placeholder)

--- src/app/newRoute/page.tsx
+++ src/app/newRoute/page.tsx
@@ -3,15 +3,15 @@ export default function Home() {
     <div className="mt-10">
       <div className="items-center justify-between lg:flex">
         <h1>
-          Welcome home!&nbsp;
+          New Route!&nbsp;
         </h1>
-        <a className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0" href="/newRoute">
-          New Route?
+        <a className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0" href="/">
+          Go home?
         </a>
       </div>
 
       <div>
-        <h5>Home</h5>
+        <h6>New Route</h6>
       </div>
     </div>
   );
