f0dae24d8b0fa91c6b9790d411d2502ab1a1ad72
Erik MbP <alerik8@yahoo.de>
2024-02-26 00:07:17 +0100

    Add: comment with source for style tutorial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Table_home_page from "@/components/table_home_page/Table_home_page"
+
 export default function Home() {
   return (
     <div className="mt-10">
@@ -9,10 +11,20 @@ export default function Home() {
           New Route?
         </a>
       </div>
-
       <div>
-        <h5>Home</h5>
+        <Table_home_page />
       </div>
     </div>
   );
 }
+
+/* 
+How to style Mui elements on the element itself... with themes and paletts, we can do it more globally and edit later easier:
+
+import { Button } from "@mui/material";
+import { deepOrange } from "@mui/material/colors";
+
+<div>
+  <Button variant="contained" sx={{color: "black", bgcolor: deepOrange[500], "&:hover": {color: "white",bgcolor: deepOrange[600]}}}> Test </ Button>
+</div> 
+*/
