d23376b50a5921e3fcdc066e8d52d967ceed2649
Erik MbP <alerik8@yahoo.de>
2024-02-26 00:39:04 +0100

    Add: Backend project (with spring boot)

---
+++ backend/brickship-backend-core/src/main/java/de/brickshipper/brickshipbackendcore/BrickshipBackendCoreApplication.java
@@ -0,0 +1,13 @@
+package de.brickshipper.brickshipbackendcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickshipBackendCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickshipBackendCoreApplication.class, args);
+	}
+
+}
---
+++ backend/brickship-backend-core/src/test/java/de/brickshipper/brickshipbackendcore/BrickshipBackendCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package de.brickshipper.brickshipbackendcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickshipBackendCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
