7581928784651e27a92f3187da79485b48517eac
Erik (PC) <alerik8@yahoo.de>
2024-02-28 19:01:20 +0100

    Edit: Changed type from enum to String to make code compatible with pre built table (with pgAdmin 4)

--- backend/brickship-backend-core/src/main/java/de/brickshipper/brickshipbackendcore/order/Order.java
+++ backend/brickship-backend-core/src/main/java/de/brickshipper/brickshipbackendcore/order/Order.java
@@ -7,7 +7,7 @@ import lombok.Data;
 
 @Entity //
 @Data   // Generates getters, setters, hashCode(), equals and .toString() methods for you
-@Table(name = "brick_shiper_api_db")
+@Table(name = "bl_orders")
 public class Order {
     @Id
     // The @Column(name="column_name") annotation does not work here and does not let some fields take any data input
@@ -16,12 +16,12 @@ public class Order {
     private String first_name;
     private String last_name;
     private String full_name;
-    private OrderStatus order_status;
+    private String order_status;
     private String order_remark;
     private String order_date;
     private String order_price;
 
-    public Order(Long order_id, String first_name, String last_name, String full_name, OrderStatus order_status, String order_remark, String order_date, String order_price) {
+    public Order(Long order_id, String first_name, String last_name, String full_name, String order_status, String order_remark, String order_date, String order_price) {
         this.order_id = order_id;
         this.first_name = first_name;
         this.last_name = last_name;
