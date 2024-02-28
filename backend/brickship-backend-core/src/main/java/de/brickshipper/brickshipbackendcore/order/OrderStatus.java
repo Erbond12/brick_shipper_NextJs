8585d8cabab79dc8bd3eee4e67528982b980613b
Erik (PC) <alerik8@yahoo.de>
2024-02-28 15:40:36 +0100

    Add: An enumerator for the different types of the order status

---
+++ backend/brickship-backend-core/src/main/java/de/brickshipper/brickshipbackendcore/order/OrderStatus.java
@@ -0,0 +1,19 @@
+package de.brickshipper.brickshipbackendcore.order;
+
+public enum OrderStatus {
+    PENDING,
+    UPDATE,
+    PROCESSING,
+    READY,
+    PAID,
+    PACKED,
+    SHIPPED,
+    RECEIVED,
+    COMLETED,
+    OCR,
+    NPB,
+    NPX,
+    NRS,
+    NSS,
+    CANCELLED
+}
